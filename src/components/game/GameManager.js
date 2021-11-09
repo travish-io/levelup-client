9637e2d274ccfa98c6946315e161d535bf77b028
Travis Hall <t.hall89d@gmail.com>
2021-11-09 16:22:30 -0500

    commit

---
+++ src/components/game/GameManager.js
@@ -0,0 +1,7 @@
+export const getGames = () => {
+  return fetch("http://localhost:8000/games", {
+    headers: {
+      Authorization: `Token ${localStorage.getItem("lu_token")}`,
+    },
+  }).then((response) => response.json());
+};
