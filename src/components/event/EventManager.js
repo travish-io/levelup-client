d16ad507b36e7c82507d3c8cf77842f1494980a4
Travis Hall <t.hall89d@gmail.com>
2021-11-12 16:50:11 -0500

    join / leave events

--- src/components/event/EventManager.js
+++ src/components/event/EventManager.js
@@ -16,3 +16,23 @@ export const getEvents = () => {
     },
   }).then((response) => response.json());
 };
+
+export const joinEvent = (eventId) => {
+  return fetch(`http://localhost:8000/events/${eventId}/signup`, {
+    method: "POST",
+    headers: {
+      Authorization: `Token ${localStorage.getItem("lu_token")}`,
+    },
+  })
+    .then((response) => response.json())
+    .then(getEvents);
+};
+
+export const leaveEvent = (eventId) => {
+  return fetch(`http://localhost:8000/events/${eventId}/signup`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Token ${localStorage.getItem("lu_token")}`,
+    },
+  });
+};
